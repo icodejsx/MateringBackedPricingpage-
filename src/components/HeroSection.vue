1acbc89b8baece9d488983150fc710e2283b2c1e
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2024-03-03 16:28:29 +0100

    almost done 97% done

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>Learn to code, grow your skills, and succeed in your tech interview</h1>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,368 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex flex-col gap-20 items-center justify-center p-4 lg:pt-11 bg-[#e2e0e0]">
+    <div class=" flex flex-col items-center gap-4     ">
+      <h1 class=" font-bold  lg:text-5xl text-3xl text-center lg:w-[60rem] text-[#17154B]">
+        Learn to code, grow your skills, and succeed in your tech interview
+      </h1>
+      <p class="lg:text-2xl text-center">Lock in your special offer! Get a head start on your 2024 career goals.</p>
+      <div class="flex lg:flex-row flex-col gap-3 items-center bg-[#17154B] p-2 text-white py-2 lg:px-10">
+        <div class="flex items-center gap-2">
+          <img alt="Vue logo" class="logo" src="@/assets/first.svg" width="20" height="" />
+          <p class="bg-[#17154B] text-sm ">Over 1,080 Courses, Projects & CloudLabs</p>
+        </div>
+        <div class="flex items-center gap-2 text-sm">
+          <img alt="Vue logo" class="logo" src="@/assets/user.svg" width="20" height="" />
+          <p class="bg-[#17154B] ">2.2 Million Learners</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-16 items-center w-full ">
+      <!-- <h1 class="text-4xl font-bold "> Our Pricing </h1> -->
+
+      <h1 class="text-4xl lg:text-5xl  font-bold text-[#17154B] -">Our Pricing </h1>
+
+      <section class=" lg:grid grid-cols-3 flex flex-col  w-full text-white gap-5  lg:px-16">
+        <div class="flex flex-col bg-[#17154B] rounded-xl ">
+          <div class="col-span-1 flex flex-col px-9 py-10 gap-5  items-center  w-full">
+            <h1 class="font-bold">Standard</h1>
+            <p class="text-2xl font-bold">$7/month</p>
+            <p class="flex gap-3">discounted price for
+              <img alt="Vue logo" class="logo" src="@/assets/ng.svg" width="20" height="" />
+            </p>
+            <p>billed annually ($84)</p>
+            <button class=" border border-white text-white  font-bold text-lg py-2 lg:px-24 px-20">
+              Get Started
+            </button>
+            <div class="flex items-center gap-2">
+              <p class=" underline">Choose 2-year plan
+
+              </p>
+              <p class="rounded-full px-4 py-1  bg-blue-400 border text-sm"> save 10%</p>
+            </div>
+            <div>
+              <p>Access over 700 interactive courses and proven skill assessments.</p>
+            </div>
+            <div class="flex  flex-col gap-8 text-sm">
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                12 months of access
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                Completion certificates
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo text-white" src="@/assets/check.svg" width="20" height="" />
+                Early access to new courses every week
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                Early access to new courses every week
+              </div>
+            </div>
+          </div>
+          <button class="flex items-center gap-4 border-t border-white w-ful px-16 py-6">
+            <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+
+            Gift a Subsription
+          </button>
+        </div>
+
+        <div class="flex flex-col bg-[#17154B] rounded-xl ">
+          <div class="col-span-1 flex flex-col px-9 py-10 gap-5  items-center  w-full">
+            <h1 class="font-bold">Standard</h1>
+            <p class="text-2xl font-bold">$7/month</p>
+            <p class="flex gap-3">discounted price for
+              <img alt="Vue logo" class="logo" src="@/assets/ng.svg" width="20" height="" />
+            </p>
+            <p>billed annually ($84)</p>
+            <button class=" border border-white text-white  font-bold text-lg py-2 lg:px-24 px-20">
+              Get Started
+            </button>
+            <div class="flex items-center gap-2">
+              <p class=" underline">Choose 2-year plan
+
+              </p>
+              <p class="rounded-full px-4 py-1  bg-blue-400 border text-sm"> save 10%</p>
+            </div>
+            <div>
+              <p>Access over 700 interactive courses and proven skill assessments.</p>
+            </div>
+            <div class="flex  flex-col gap-8 text-sm">
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                12 months of access
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                Completion certificates
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo text-white" src="@/assets/check.svg" width="20" height="" />
+                Early access to new courses every week
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                Early access to new courses every week
+              </div>
+            </div>
+          </div>
+          <button class="flex items-center gap-4 border-t border-white w-ful px-16 py-6">
+            <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+
+            Gift a Subsription
+          </button>
+        </div>
+
+        <div class="flex flex-col bg-[#17154B] rounded-xl ">
+          <div class="col-span-1 flex flex-col px-9 py-10 gap-5  items-center  w-full">
+            <h1 class="font-bold">Standard</h1>
+            <p class="text-2xl font-bold">$7/month</p>
+            <p class="flex gap-3">discounted price for
+              <img alt="Vue logo" class="logo" src="@/assets/ng.svg" width="20" height="" />
+            </p>
+            <p>billed annually ($84)</p>
+            <button class=" border border-white text-white  font-bold text-lg py-2 lg:px-24 px-20">
+              Get Started
+            </button>
+            <div class="flex items-center gap-2">
+              <p class=" underline">Choose 2-year plan
+
+              </p>
+              <p class="rounded-full px-4 py-1  bg-blue-400 border text-sm"> save 10%</p>
+            </div>
+            <div>
+              <p>Access over 700 interactive courses and proven skill assessments.</p>
+            </div>
+            <div class="flex  flex-col gap-8 text-sm">
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                12 months of access
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                Completion certificates
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo text-white" src="@/assets/check.svg" width="20" height="" />
+                Early access to new courses every week
+              </div>
+              <div class="flex items-center gap-6">
+                <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+                Early access to new courses every week
+              </div>
+            </div>
+          </div>
+          <button class="flex items-center gap-4 border-t border-white w-ful px-16 py-6">
+            <img alt="Vue logo" class="logo" src="@/assets/check.svg" width="20" height="" />
+
+            Gift a Subsription
+          </button>
+        </div>
+      </section>
+
+      <section class=" flex items-center justify-between w-full flex-wrap px-4 lg:px-20">
+        <h1>CHOSEN BY LEARNERS WORKING AT</h1>
+        <img alt="Vue logo" class="logo" src="@/assets/meta.png" width="100" height="" />
+        <img alt="Vue logo" class="logo" src="@/assets/amazon.png" width="100" height="" />
+        <img alt="Vue logo" class="logo" src="@/assets/apple.png" width="100" height="" />
+        <img alt="Vue logo" class="logo" src="@/assets/net.png" width="100" height="" />
+        <img alt="Vue logo" class="logo" src="@/assets/google.png" width="100" height="" />
+
+      </section>
+
+      <section class="flex flex-col gap-16 lg:px-20 px-4">
+        <h1 class="font-bold text-4xl text-[#17154B]  ">Why Mastering Backend?</h1>
+        <div class="lg:grid flex flex-col  grid-cols-2 gap-16 lg:gap-10">
+          <div class="col-span-1 flex flex-col gap-6 ">
+            <img alt="Vue logo" class="logo" src="@/assets/strike.svg" width="60" height="" />
+            <div class="flex flex-col gap-1">
+
+              <h1 class="font-bold text-3xl text-gray-800">Learn Interactively</h1>
+              <p class="text-gray-700 text-2xl">Our courses include built-in coding playgrounds that let you learn new
+                things
+                without
+                any setup.</p>
+            </div>
+          </div>
+
+          <div class="col-span-1 flex flex-col gap-6 ">
+            <img alt="Vue logo" class="logo" src="@/assets/strike.svg" width="60" height="" />
+            <div class="flex flex-col gap-1">
+              <h1 class="font-bold text-2xl text-gray-800">Learn Faster</h1>
+              <p class="text-gray-700 text-2xl">All of our learning products are text-based. You get to learn at your own
+                pace. No pauses.</p>
+            </div>
+          </div>
+
+          <div class="col-span-1 flex flex-col gap-6 ">
+            <img alt="Vue logo" class="logo" src="@/assets/strike.svg" width="60" height="" />
+            <div class="flex flex-col gap-1">
+              <h1 class="font-bold text-2xl text-gray-800">Personalize Your Learning</h1>
+              <p class="text-gray-700 text-2xl">Achieve your goals faster with a path designed just for you. Personalized
+                Paths are customized and focused on your individual learning needs and career goals.</p>
+            </div>
+          </div>
+          <div class="col-span-1 flex flex-col gap-6 ">
+            <img alt="Vue logo" class="logo" src="@/assets/strike.svg" width="60" height="" />
+            <div class="flex flex-col gap-1">
+              <h1 class="font-bold text-2xl text-gray-800">Build Real-World Projects</h1>
+              <p class="text-gray-700 text-2xl">Complete a real-world programming project designed to exercise practical
+                skills used in the workplace. Everything you learn from a Project will help you practice skills that are
+                in-demand, useful, and highly relevant.</p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="flex flex-col gap-16 mt-10 lg:px-20 px-4">
+        <h1 class="lg:text-4xl text-3xl font-bold text-[#17154B] -">What Our Learners Say</h1>
+        <div class="lg:grid  flex flex-col grid-cols-3 gap-10">
+          <div class="flex flex-col md:gap-8 gap-9 items-center bg-[#17154B] text-white p-10 rounded-2xl">
+            <h1 class="md:text-xl text text-start">Lorem ipsum dolor sit amet consectetur. Sagittis eu tellus
+              turpis
+              diam accumsan
+              ut
+              condimentum. Eget
+              dignissim dictum arcu eget cras sit vulputate. Ante enim non aliquet nibh senectus adipiscing nunc. Ut.
+            </h1>
+
+            <div class="flex f justify-start items-center w-full gap-2">
+              <img src="@/assets/use2.png" alt="" class="w-16" />
+              <div>
+                <h1 class="text-xl font-semibold">Angela Karamoy</h1>
+                <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col md:gap-8 gap-9 items-center bg-[#17154B] text-white p-10 rounded-2xl">
+            <h1 class="md:text-xl text text-start">Lorem ipsum dolor sit amet consectetur. Sagittis eu tellus
+              turpis
+              diam accumsan
+              ut
+              condimentum. Eget
+              dignissim dictum arcu eget cras sit vulputate. Ante enim non aliquet nibh senectus adipiscing nunc. Ut.
+            </h1>
+
+            <div class="flex f justify-start items-center w-full gap-2">
+              <img src="@/assets/use2.png" alt="" class="w-16" />
+              <div>
+                <h1 class="text-xl font-semibold">Angela Karamoy</h1>
+                <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col md:gap-8 gap-9 items-center bg-[#17154B] text-white p-10 rounded-2xl">
+            <h1 class="md:text-xl text text-start">Lorem ipsum dolor sit amet consectetur. Sagittis eu tellus
+              turpis
+              diam accumsan
+              ut
+              condimentum. Eget
+              dignissim dictum arcu eget cras sit vulputate. Ante enim non aliquet nibh senectus adipiscing nunc. Ut.
+            </h1>
+
+            <div class="flex f justify-start items-center w-full gap-2">
+              <img src="@/assets/use2.png" alt="" class="w-16" />
+              <div>
+                <h1 class="text-xl font-semibold">Angela Karamoy</h1>
+                <p>Lorem ipsum dolor sit amet</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+
+      <section class="flex lg:flex-row flex-col gap-10  items-center justify-between  w-full lg:px-20 px-4 mt-10">
+        <div class="flex flex-col gap-4 lg:w-96">
+
+          <h1 class="lg:text-4xl text-3xl font-bold text-[#17154B] -">Show off your skills with Certificates</h1>
+          <p>Built-in assessments let you test your skills. Completion certificates let you show them off.
+          </p>
+        </div>
+        <img alt="Vue logo" class="logo" src="@/assets/qq.jpg" width="600" height="" />
+      </section>
+
+      <section
+        class="flex lg:flex-row flex-col gap-10 items-center justify-between m-20 text-white lg:px-20 px-4 py-16 bg-[#17154B] w-full rounded-2xl shadow-2xl">
+        <div class=" flex flex-col gap-10">
+          <div class="flex flex-col gap-2">
+
+            <h1 class="font-bold text-xl">Anything you'd like to ask?</h1>
+            <p class="text-gray-400">We're happy to chat.</p>
+          </div>
+
+          <div class="flex flex-col gap-2">
+            <p class="text-sm">EMAIL US AT</p>
+            <p>SUPPORT@MASTERINGBACKEND.COM</p>
+          </div>
+          <div class="inline">
+            <button class="px-4 py-2 bg-white text-[#17154B] inline">
+              Send an Email
+            </button>
+          </div>
+
+        </div>
+
+        <img alt="Vue logo" class="logo" src="@/assets/email.png" width="400" height="" />
+      </section>
+    </div>
+
+    <footer class="w-full bg-white ">
+      <div class="md:p-20 pt-4  w-full  ">
+
+        <div class="grid lg:grid-cols-2 gap-20 lg:gap-0 w-full">
+          <div class="col-span-1 flex flex-col gap-4">
+            <img src="@/assets/mastering.png" class="w-64">
+            <p class="text- md:w-96">Sometimes features require a short description. This can be detailed description or
+              just
+              a short text.
+            </p>
+          </div>
+          <div class="col-span-1 grid grid-cols-3">
+            <div class="col-span-1 flex flex-col gap-4">
+              <h1 class=" font-bold">Company</h1>
+              <div class="flex flex-col gap-4 text-gray-400">
+                <p>About</p>
+                <p>Community Supports</p>
+                <p>Events</p>
+                <p>Career</p>
+              </div>
+            </div>
+
+            <div class="col-span-1 flex flex-col gap-4">
+              <h1 class=" font-bold">Support</h1>
+              <div class="flex flex-col gap-4 text-gray-400">
+                <p>Account</p>
+                <p>Support center </p>
+                <p>FeddBack</p>
+                <p>Contact Us</p>
+              </div>
+            </div>
+
+            <div class="col-span-1 flex flex-col gap-4">
+              <h1 class=" font-bold">Resourse</h1>
+              <div class="flex flex-col gap-4 text-gray-400">
+                <p>Help Center</p>
+                <p>Guide</p>
+                <p>Blog</p>
+                <p>Partner Network</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </footer>
   </main>
 </template>
